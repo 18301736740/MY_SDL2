3e9c1c3ed61470ef458c4956c23d148cf3178ad7
wangjianqun200 <wangjianqun200@163.com>
2020-03-14 16:25:01 +0800

    修复Player不能正常退出的问题

--- qun_player/qun_player.cpp
+++ qun_player/qun_player.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define __STDC_CONSTANT_MACROS
- 
+
 #ifdef _WIN32
 //Windows
 extern "C"
@@ -38,10 +38,10 @@ bool thread_pause = false;
 // Thread 每40s响应一次SFM_REFRESH_EVENT事件
 int sfp_refresh_thread(void * opaque)
 {
-    
+
 	thread_exit = false;
 	thread_pause = false;
-    
+
     SDL_Event event;
     while(!thread_exit)
     {
@@ -54,7 +54,7 @@ int sfp_refresh_thread(void * opaque)
     }
     thread_exit = false;
     thread_pause = false;
-    
+
     // break
     event.type = SFM_BREAK_EVENT;
     SDL_PushEvent(&event);
@@ -91,7 +91,7 @@ int my_av_read_frame(
 				printf("Decode Error.\n");
 				return -1;
 			}
-    
+
     if(got_picture)
     {
         sws_scale(img_convert_ctx,(const uint8_t* const*)pFrame->data,pFrame->linesize,0,pCodecCtx->height,pFrameYUV->data,pFrameYUV->linesize);
@@ -122,26 +122,26 @@ int main(int argc,char *argv[])
     SDL_Event         event;
     int               screen_w = 0,screen_h = 0;
     int               i,videoindex = -1;
-        
+
     //avdevice_register_all();
     avformat_network_init();
     pFrame = av_frame_alloc();
     pFrameYUV = av_frame_alloc();
     pFormatCtx = avformat_alloc_context();
     packet = (AVPacket *)av_malloc(sizeof(AVPacket));
-    
+
     if(0 != avformat_open_input(&pFormatCtx,argv[1],NULL,NULL))
     {
         printf("couldn't open input stream.\n");
         return -1;
     }
-    
+
     if(avformat_find_stream_info(pFormatCtx,NULL) < 0)
     {
         printf("couldn't find stream information.\n");
         return -1;
     }
-    
+
     for(i = 0;i < pFormatCtx->nb_streams;i++)
     {
         if(pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
@@ -150,13 +150,13 @@ int main(int argc,char *argv[])
             break;
         }
     }
-    
+
     if(-1 == videoindex)
     {
         printf("Didn't find a video stream.\n");
         return -1;
     }
-    
+
     pCodecCtx = pFormatCtx->streams[videoindex]->codec;
     pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
     if(pCodec == NULL)
@@ -164,22 +164,22 @@ int main(int argc,char *argv[])
         printf("Codec not found.\n");
         return -1;
     }
-    
+
     if(avcodec_open2(pCodecCtx,pCodec,NULL) < 0)
     {
         printf("could not open codec.\n");
         return -1;
     }
-    
+
     out_buffer=(unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P,  pCodecCtx->width, pCodecCtx->height,1));
     av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize,out_buffer,AV_PIX_FMT_YUV420P,pCodecCtx->width, pCodecCtx->height,1);
-    
+
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER))
     {
         printf("could not initialize SDL - %s\n",SDL_GetError());
         return -1;
     }
-    
+
     screen_w = pCodecCtx->width;
     screen_h = pCodecCtx->height;
     screen = SDL_CreateWindow("qun_player", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,screen_w, screen_h,SDL_WINDOW_OPENGL);
@@ -189,25 +189,25 @@ int main(int argc,char *argv[])
         printf("SDL: could not set video mode - exiting:%s\n");
         return -1;
     }
-    
+
     sdlRenderer = SDL_CreateRenderer(screen, -1, 0);
-    
+
     sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING,pCodecCtx->width,pCodecCtx->height);
-    
+
     sdlRect.x = 0;
     sdlRect.y = 0;
     sdlRect.w = screen_w;
     sdlRect.h = screen_h;
-    
+
     printf("------------File Information-----------\n");
     av_dump_format(pFormatCtx,0,argv[1],0);
     printf("-----------------End--------------------\n");
-    
+
 	// 初始化sws_scale
     img_convert_ctx = sws_getContext(screen_w,screen_h,pCodecCtx->pix_fmt,screen_w,screen_h,AV_PIX_FMT_YUV420P,SWS_BICUBIC,NULL,NULL,NULL);
-    
+
     video_tid = SDL_CreateThread(sfp_refresh_thread,NULL,NULL);
-    
+
     for(;;)
     {
         SDL_WaitEvent(&event);
@@ -223,21 +223,22 @@ int main(int argc,char *argv[])
                 break;
             }
             case SDL_QUIT:
-                thread_exit=1;
-                break;
+                thread_exit = true;
+                goto quit;
             case SFM_BREAK_EVENT:
                 break;
             default:
                 printf("could not know the event\n");
         }
     }
-    
+
+quit:
     SDL_Quit();
     sws_freeContext(img_convert_ctx);
     av_frame_free(&pFrameYUV);
     av_frame_free(&pFrame);
     avcodec_close(pCodecCtx);
     avformat_close_input(&pFormatCtx);
-    
+
     return 0;
 }
